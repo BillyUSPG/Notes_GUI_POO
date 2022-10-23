11dc329313409a0f998d5d85e1c354a1c11799dc
BillyUSPG <109440762+BillyUSPG@users.noreply.github.com>
2022-10-23 11:22:50 -0600

    Add files via upload

---
+++ GUI_POO/src/main/java/com/mycompany/gui_poo/GUI_POO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gui_poo;
+
+/**
+ *
+ * @author Billy
+ */
+public class GUI_POO {
+
+    public static void main(String[] args) {
+        Notes i = new Notes();
+        i.setVisible(true);
+    }
+}
